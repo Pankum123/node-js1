07f50a62821279c0a8951f1552fb6fc1cc790e11
Pankum123 <pka936636@gmail.com>
2024-10-10 22:17:41 +0530

    Add files via upload

---
+++ Fundamentals/index.js
@@ -0,0 +1,17 @@
+const app = require('./app');
+
+console.log(app)
+console.log(app.x)
+console.log(app.y)
+console.log(app.z())
+
+arr = [1,2,3,4,5,6,7,8];
+
+// arr.filter((item)=>{
+//     console.log(item);
+// })
+
+// let result = arr.filter((item)=>{
+//     return item >=4;
+// })
+// console.log(result);
