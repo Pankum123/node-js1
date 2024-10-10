db44302bf3063c16da371ae8cf9b55ca33479bec
Pankum123 <pka936636@gmail.com>
2024-10-10 22:32:30 +0530

    Add files via upload

---
+++ core-module/index.js
@@ -0,0 +1,9 @@
+// const fs = require("fs");  // fs - file system
+// fs.writeFileSync("hello.txt","code step by step")
+// fs.writeFileSync("hello.txt2","like and subscribe")
+
+const fs = require("fs").writeFileSync;
+fs("abc.txt","abcdefghi");
+
+// console.log("->>",__dirname);
+// console.log("->>",__filename);
